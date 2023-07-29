7cc28e51e25b5c37e39c28c2e6b4a4d17d9c0b29
jrsanchezmu <jrsanchezmu@gmail.com>
2023-07-29 16:14:34 -0500

    reto 3

---
+++ src/test/java/com/desaextremo/retocuatro/RetodosApplicationTests.java
@@ -0,0 +1,11 @@
+package com.desaextremo.retocuatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetodosApplicationTests {
+	//@Test
+	void contextLoads() {
+	}
+}
